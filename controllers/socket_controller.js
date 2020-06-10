19e5ac890f32037844d9dc645158bdaad6ecef13
MyNa Do <myna.do@mahoyo.com>
2020-06-10 14:59:01 +0200

    added layout for gameboard and scoreboard

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -3,11 +3,37 @@
  */
 
 const debug = require('debug')('kill-the-virus:socket_controller');
+const users = {};
+
+/**
+ * Get username of online players
+ */
+function getOnlinePlayers() {
+	return Object.values(users);
+}
+
+function handleUserDisconnect() {
+	debug('Someone left the game.');
+}
+
+function handleRegisterUser(username, callback) {
+	debug(`Player ${username} is connected to the game.`);
+	users[this.id] = username;
+	callback({
+		joinGame: true,
+		usernameInUse: false,
+		onlinePlayers: getOnlinePlayers(),
+	});
+
+	// broadcast to all connected sockets except ourselves
+	this.broadcast.emit('online-players', getOnlinePlayers());
+
+}
 
 module.exports = function(socket) {
 	debug('A player connected!', socket.id);
 
-	socket.on('disconnect', () => {
-		debug('Someone left the game.');
-	});
+	socket.on('disconnect', handleUserDisconnect);
+
+	socket.on('register-user', handleRegisterUser);
 }
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -4,3 +4,17 @@
 
 // make connection
 const socket = io();
+
+const startEl = document.querySelector('#start');
+const usernameForm = document.querySelector('#username-form');
+
+// get username and emit register-user-event to server
+usernameForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	username = document.querySelector('#username').value;
+	socket.emit('register-user', username, (status) => {
+		console.log('Server acknowledge the registration');
+
+	})
+})
